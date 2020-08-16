e9fae5b0a42d993c7beb72ee2e7560878e0ee525
fogjama <66557609+fogjama@users.noreply.github.com>
2020-08-16 22:07:19 +0100

    Set template for models

--- models.py
+++ models.py
@@ -1,1 +1,20 @@
+import os
+from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
 
+database_path = os.environ['DATABASE_URL']
+
+db = SQLAlchemy()
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+'''
+@TODO: Create models
+'''
