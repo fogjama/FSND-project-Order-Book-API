2144d07aac0cadb43065faee96c78a571a7228eb
fogjama <66557609+fogjama@users.noreply.github.com>
2020-08-16 21:58:22 +0100

    Set template for auth

---
+++ auth/auth.py
@@ -0,0 +1,62 @@
+import os
+
+auth0_domain = os.environ['AUTH0_DOMAIN']
+api_audience = os.environ['API_AUDIENCE']
+algorithms = ['RS256']
+
+
+# Autherror exception
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+
+# Auth Header
+
+'''
+@TODO implement get_token_auth_header() method
+    it should attempt to get the header from the request
+        it should raise an AuthError if no header is present
+    it should attempt to split bearer and the token
+        it should raise an AuthError if the header is malformed
+    return the token part of the header
+'''
+
+'''
+@TODO implement check_permissions(permission, payload) method
+    @INPUTS
+        permission: string permission
+        payload: decoded jwt payload
+
+    it should raise an AuthError if permissions are not included in the payload
+        !!NOTE check your RBAC settings in Auth0
+    it should raise an AuthError if the requested permission string is not in the payload permissions array
+    return true otherwise
+'''
+
+'''
+@TODO implement verify_decode_jwt(token) method
+    @INPUTS
+        token: a json web token (string)
+
+    it should be an Auth0 token with key id (kid)
+    it should verify the token using Auth0 /.well-known/jwks.json
+    it should decode the payload from the token
+    it should validate the claims
+    return the decoded payload
+
+    !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
+'''
+
+'''
+@TODO implement @requires_auth(permission) decorator method
+    @INPUTS
+        permission: string permission
+
+    it should use the get_token_auth_header method to get the token
+    it should use the verify_decode_jwt method to decode the jwt
+    it should use the check_permissions method validate claims and check the requested permission
+    return the decorator which passes the decoded payload to the decorated method
+'''
