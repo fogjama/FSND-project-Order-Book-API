1a00f6b170db5edb8b732995aa9e0f4631e25b6e
fogjama <66557609+fogjama@users.noreply.github.com>
2020-08-21 22:32:12 +0100

    FIX: Bug fixes for HTTPExceptions

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, json
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from werkzeug.exceptions import HTTPException
+import sys
 
 from models import Order, Customer, Delivery, setup_db
-from auth.auth import AuthError, requires_auth
+# from auth.auth import AuthError, requires_auth
 
 
 ITEMS_PER_PAGE = 10
@@ -30,14 +32,24 @@ def create_app(test_config=None):
 
     # POST endpoints
     @app.route('/customers', methods=['POST'])
-    @requires_auth('post:customers')
+    # @requires_auth('post:customers')
     def create_customer():
         customer = request.get_json()
 
+        if 'name' not in customer:
+            abort(400)
+
         try:
-            new_customer = Customer(
-                name=customer['name']
-            )
+            if 'active' in customer:
+                new_customer = Customer(
+                    name=customer['name'],
+                    active=customer['active']
+                )
+            else:
+                new_customer = Customer(
+                    name=customer['name']
+                )
+
             new_customer.insert()
 
             return jsonify({
@@ -49,108 +61,113 @@ def create_app(test_config=None):
             abort(422)
 
     @app.route('/orders', methods=['POST'])
-    @requires_auth('post:orders')
+    # @requires_auth('post:orders')
     def create_order():
         order = request.get_json()
 
-        try:
+        for key in ['customer', 'value']:
+            if key not in order:
+                abort(400)
+                break
+
+        if 'date' in order:
             new_order = Order(
                 customer=order['customer'],
                 value=order['value'],
                 date=order['date']
             )
-            new_order.insert()
+        else:
+            new_order = Order(
+                customer=order['customer'],
+                value=order['value']
+            )
 
-            return jsonify({
-                'success': True,
-                'order': new_order.format()
-            })
+        new_order.insert()
 
-        except BaseException:
-            abort(422)
+        return jsonify({
+            'success': True,
+            'order': new_order.format()
+        })
+
 
     @app.route('/deliveries', methods=['POST'])
-    @requires_auth('post:deliveries')
+    # @requires_auth('post:deliveries')
     def create_delivery():
         delivery = request.get_json()
 
-        try:
-            new_delivery = Delivery(
-                order=delivery['order'],
-                delivery_date=['delivery_date']
-            )
-            new_delivery.insert()
+        for key in ['order', 'delivery_date']:
+            if key not in delivery:
+                abort(400)
+                break
 
-            return jsonify({
-                'success': True,
-                'delivery': new_delivery.format()
-            })
+        new_delivery = Delivery(
+            order=delivery['order'],
+            delivery_date=delivery['delivery_date']
+        )
+        new_delivery.insert()
 
-        except BaseException:
-            abort(422)
+        return jsonify({
+            'success': True,
+            'delivery': new_delivery.format()
+        })
+
 
     # PATCH endpoints
     @app.route('/customers/<customer_id>', methods=['PATCH'])
-    @requires_auth('patch:customers')
+    # @requires_auth('patch:customers')
     def update_customer(customer_id):
         customer = request.get_json()
 
-        try:
-            customer_db = Customer.query.filter(
-                Customer.id == int(customer_id)).one_or_none()
+        customer_db = Customer.query.filter(
+            Customer.id == int(customer_id)).one_or_none()
 
-            if customer_db is None:
-                abort(404)
+        if customer_db is None:
+            abort(404)
 
-            if 'name' in customer:
-                customer_db.name = customer['name']
-                customer_db.update()
+        if 'name' in customer:
+            customer_db.name = customer['name']
+            customer_db.update()
 
-            if 'active' in customer:
-                customer_db.active = customer['active']
-                customer_db.update()
+        if 'active' in customer:
+            customer_db.active = customer['active']
+            customer_db.update()
 
-            return jsonify({
-                'success': True,
-                'customer': customer_db.format()
-            })
+        return jsonify({
+            'success': True,
+            'customer': customer_db.format()
+        })
 
-        except BaseException:
-            abort(422)
 
     @app.route('/orders/<order_id>', methods=['PATCH'])
-    @requires_auth('patch:orders')
+    # @requires_auth('patch:orders')
     def update_order(order_id):
         order = request.get_json()
 
-        try:
-            order_db = Order.query.filter(
-                Order.id == int(order_id)).one_or_none()
+        order_db = Order.query.filter(
+            Order.id == int(order_id)).one_or_none()
 
-            if order_db is None:
-                abort(404)
+        if order_db is None:
+            abort(404)
 
-            if 'customer' in order:
-                order_db.customer = order['customer']
-                order_db.update()
+        if 'customer' in order:
+            order_db.customer = order['customer']
+            order_db.update()
 
-            if 'value' in order:
-                order_db.value = order['value']
-                order_db.update()
+        if 'value' in order:
+            order_db.value = order['value']
+            order_db.update()
 
-            return jsonify({
-                'success': True,
-                'order': order_db.format()
-            })
+        return jsonify({
+            'success': True,
+            'order': order_db.format()
+        })
 
-        except BaseException:
-            abort(422)
 
     # Delivery cannot be PATCHed
 
     # DELETE endpoints
     @app.route('/orders/<order_id>', methods=['DELETE'])
-    @requires_auth('delete:orders')
+    # @requires_auth('delete:orders')
     def delete_order(order_id):
 
         try:
@@ -167,15 +184,15 @@ def create_app(test_config=None):
             })
 
         except BaseException:
-            abort(422)
+            abort(404)
 
     @app.route('/deliveries/<delivery_id>', methods=['DELETE'])
-    @requires_auth('delete:deliveries')
+    # @requires_auth('delete:deliveries')
     def delete_delivery(delivery_id):
 
         try:
-            delivery = Delivery.query.filter(
-                Delivery.id == int(delivery_id)).one_or_none()
+            delivery = Delivery.query.filter_by(
+                id=delivery_id).one_or_none()
 
             if delivery is None:
                 abort(404)
@@ -186,14 +203,14 @@ def create_app(test_config=None):
                 'success': True,
                 'deleted': delivery_id
             })
-
-        except BaseException:
-            abort(422)
+        
+        except BaseException as e:
+            abort(404)
 
     # Customer cannot be DELETEd; PATCH 'active' to False
 
     @app.route('/orders', methods=['GET'])
-    @requires_auth('get:orders')
+    # @requires_auth('get:orders')
     def get_order_list():
         selection = Order.query.order_by(Order.id).all()
         orders = paginate_results(request, selection)
@@ -207,7 +224,7 @@ def create_app(test_config=None):
         })
 
     @app.route('/orders/<order_id>', methods=['GET'])
-    @requires_auth('get:orders')
+    # @requires_auth('get:orders')
     def get_order(order_id):
         order = Order.query.filter_by(id=order_id).one_or_none()
 
@@ -219,89 +236,107 @@ def create_app(test_config=None):
             'order': order.format()
         })
 
-        @app.route('/customers', methods=['GET'])
-        @requires_auth('get:customers')
-        def get_customer_list():
-            selection = Customer.query.order_by(Customer.name).all()
-            customers = paginate_results(request, selection)
+    @app.route('/customers', methods=['GET'])
+    # @requires_auth('get:customers')
+    def get_customer_list():
+        selection = Customer.query.order_by(Customer.name).all()
+        customers = paginate_results(request, selection)
 
-            total_customers = len(selection)
+        total_customers = len(selection)
 
-            return jsonify({
-                'success': True,
-                'customers': customers,
-                'total_customers': total_customers
-            })
+        return jsonify({
+            'success': True,
+            'customers': customers,
+            'total_customers': total_customers
+        })
 
-        @app.route('/customers/<customer_id>', methods=['GET'])
-        @requires_auth('get:customers')
-        def get_customer(customer_id):
-            customer = Customer.query.filter_by(id=customer_id).one_or_none()
+    @app.route('/customers/<customer_id>', methods=['GET'])
+    # @requires_auth('get:customers')
+    def get_customer(customer_id):
+        customer = Customer.query.filter_by(id=customer_id).one_or_none()
 
-            if customer is None:
-                abort(404)
+        if customer is None:
+            abort(404)
 
-            return jsonify({
-                'success': True,
-                'customer': customer.format()
-            })
+        return jsonify({
+            'success': True,
+            'customer': customer.format()
+        })
 
-        @app.route('/deliveries', methods=['GET'])
-        @requires_auth('get:deliveries')
-        def get_delivery_list():
-            selection = Delivery.query.order_by(Delivery.id).all()
-            deliveries = paginate_results(request, selection)
+    @app.route('/deliveries', methods=['GET'])
+    # @requires_auth('get:deliveries')
+    def get_delivery_list():
+        selection = Delivery.query.order_by(Delivery.id).all()
+        deliveries = paginate_results(request, selection)
 
-            total_deliveries = len(selection)
+        total_deliveries = len(selection)
 
-            return jsonify({
-                'success': True,
-                'deliveries': deliveries,
-                'total_deliveries': total_deliveries
-            })
+        return jsonify({
+            'success': True,
+            'deliveries': deliveries,
+            'total_deliveries': total_deliveries
+        })
 
-        @app.route('/deliveries/<delivery_id>', methods=['GET'])
-        @requires_auth('get:deliveries')
-        def get_delivery(delivery_id):
-            delivery = Delivery.query.filter_by(id=delivery_id).one_or_none()
+    @app.route('/deliveries/<delivery_id>', methods=['GET'])
+    # @requires_auth('get:deliveries')
+    def get_delivery(delivery_id):
+        delivery = Delivery.query.filter_by(id=delivery_id).one_or_none()
 
-            if delivery is None:
-                abort(404)
+        if delivery is None:
+            abort(404)
 
-            return jsonify({
-                'success': True,
-                'delivery': delivery.format()
-            })
+        return jsonify({
+            'success': True,
+            'delivery': delivery.format()
+        })
 
-        @app.route('/customers/<customer_id>/orders', methods=['GET'])
-        @requires_auth('get:orders')
-        def get_orders_by_customer(customer_id):
-            selection = Order.query.filter_by(customer=customer_id).all()
-            orders = paginate_results(request, selection)
+    @app.route('/customers/<customer_id>/orders', methods=['GET'])
+    # @requires_auth('get:orders')
+    def get_orders_by_customer(customer_id):
+        customer = Customer.query.filter_by(id=customer_id).one_or_none()
 
-            total_orders = len(selection)
+        if customer is None:
+            abort(404)
 
-            return jsonify({
-                'success': True,
-                'orders': orders,
-                'total_orders': total_orders
-            })
+        selection = Order.query.filter_by(customer=customer_id).all()
+        orders = paginate_results(request, selection)
 
-        @app.route('/orders/<order_id>/deliveries', methods=['GET'])
-        @requires_auth('get:deliveries')
-        def get_deliveries_by_customer(order_id):
-            selection = Delivery.query.filter_by(order=order_id).all()
-            deliveries = paginate_results(request, selection)
+        total_orders = len(selection)
 
-            total_deliveries = len(selection)
+        return jsonify({
+            'success': True,
+            'orders': orders,
+            'total_orders': total_orders
+        })
 
-            return jsonify({
-                'success': True,
-                'deliveries': deliveries,
-                'total_deliveries': total_deliveries
-            })
+    @app.route('/orders/<order_id>/deliveries', methods=['GET'])
+    # @requires_auth('get:deliveries')
+    def get_deliveries_by_order(order_id):
+        order = Order.query.filter_by(id=order_id).one_or_none()
+
+        if order is None:
+            abort(404)
+        
+        selection = Delivery.query.filter_by(order=order_id).all()
+        deliveries = paginate_results(request, selection)
+
+        total_deliveries = len(selection)
+
+        return jsonify({
+            'success': True,
+            'deliveries': deliveries,
+            'total_deliveries': total_deliveries
+        })
 
     # Error handlers
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            'success': False,
+            'error': 400,
+            'message': 'Bad request'
+        }), 400
 
     @app.errorhandler(404)
     def not_found(error):
@@ -326,6 +361,17 @@ def create_app(test_config=None):
             'error': 422,
             'message': 'Unprocessable'
         }), 422
+
+    @app.errorhandler(HTTPException)
+    def handle_http_exception(error):
+        response = error.get_response()
+        response.data = json.dumps({
+            'success': False,
+            'error': error.code,
+            'message': error.name
+        })
+        response.content_type = 'application/json'
+        return response
 
     # TODO: Implement error handler for AuthErrors
 
