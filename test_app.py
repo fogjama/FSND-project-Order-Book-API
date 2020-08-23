23f9574ade341b82633b7884fd5e8f350682b7ca
fogjama <66557609+fogjama@users.noreply.github.com>
2020-08-23 18:02:37 +0100

    Implement authorisation in test sequences

--- test_app.py
+++ test_app.py
@@ -3,10 +3,16 @@ import unittest
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 import json
+import os
 
 from app import create_app
 from models import setup_db, Order, Delivery, Customer
 
+admin_token = os.environ['ADMIN_TOKEN']
+user_token = os.environ['USER_TOKEN']
+auth_admin = {'Authorization': f'Bearer {admin_token}'}
+auth_user = {'Authorization': f'Bearer {user_token}'}
+auth_malformed = {'Authorization': 'Basic'}
 
 class OrdersTestCase(unittest.TestCase):
 
@@ -47,51 +53,67 @@ class OrdersTestCase(unittest.TestCase):
     # Test Post
 
     def test_create_customer(self):
-        res = self.client().post('/customers', json=self.new_customer)
+        res = self.client().post('/customers', headers=auth_admin, json=self.new_customer)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
     def test_create_order(self):
-        res = self.client().post('/orders', json=self.new_order)
+        res = self.client().post('/orders', headers=auth_admin, json=self.new_order)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
     def test_create_delivery(self):
-        res = self.client().post('/deliveries', json=self.new_delivery)
+        res = self.client().post('/deliveries', headers=auth_admin, json=self.new_delivery)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
-    def test_create_order_error(self):
-        res = self.client().post('/orders', json={'name': False})
+    def test_create_order_bad_request(self):
+        res = self.client().post('/orders', headers=auth_admin, json={'name': False})
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 400)
         self.assertEqual(data['success'], False)
 
-    def test_create_customer_error(self):
-        res = self.client().post('/customers', json={'active': False})
+    def test_create_customer_bad_request(self):
+        res = self.client().post('/customers', headers=auth_admin, json={'active': False})
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 400)
         self.assertEqual(data['success'], False)
 
-    def test_create_delivery_error(self):
-        res = self.client().post('/deliveries', json={'name': 'wombat'})
+    def test_create_delivery_bad_request(self):
+        res = self.client().post('/deliveries', headers=auth_admin, json={'name': 'wombat'})
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+    
+    # RBAC tests on Create
+
+    def test_create_order_missing_auth(self):
+        res = self.client().post('/orders', headers=auth_user, json={'name': False})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 403)
+        self.assertEqual(data['success'], False)
+
+    def test_create_order_malformed_auth(self):
+        res = self.client().post('/orders', headers=auth_malformed, json=self.new_order)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
         self.assertEqual(data['success'], False)
 
     # Test Get
 
     def test_get_paginated_orders(self):
-        res = self.client().get('/orders')
+        res = self.client().get('/orders', headers=auth_user)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -100,7 +122,7 @@ class OrdersTestCase(unittest.TestCase):
         self.assertTrue(data['total_orders'])
 
     def test_get_order_by_id(self):
-        res = self.client().get('/orders/1')
+        res = self.client().get('/orders/1', headers=auth_user)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -111,14 +133,14 @@ class OrdersTestCase(unittest.TestCase):
         self.assertTrue(data['order']['customer'])
 
     def test_404_get_order_by_nonexistant_id(self):
-        res = self.client().get('orders/100000')
+        res = self.client().get('orders/100000', headers=auth_user)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
     def test_get_paginated_customers(self):
-        res = self.client().get('/customers')
+        res = self.client().get('/customers', headers=auth_user)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -127,7 +149,7 @@ class OrdersTestCase(unittest.TestCase):
         self.assertTrue(data['total_customers'])
 
     def test_get_customer_by_id(self):
-        res = self.client().get('/customers/1')
+        res = self.client().get('/customers/1', headers=auth_user)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -136,14 +158,14 @@ class OrdersTestCase(unittest.TestCase):
         self.assertTrue(data['customer']['name'])
 
     def test_404_get_customer_by_nonexistant_id(self):
-        res = self.client().get('/customers/1000000')
+        res = self.client().get('/customers/1000000', headers=auth_user)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
     def test_get_paginated_deliveries(self):
-        res = self.client().get('/deliveries')
+        res = self.client().get('/deliveries', headers=auth_user)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -152,7 +174,7 @@ class OrdersTestCase(unittest.TestCase):
         self.assertTrue(data['total_deliveries'])
 
     def test_get_delivery_by_id(self):
-        res = self.client().get('/deliveries/1')
+        res = self.client().get('/deliveries/1', headers=auth_user)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -162,14 +184,14 @@ class OrdersTestCase(unittest.TestCase):
         self.assertTrue(data['delivery']['delivery_date'])
 
     def test_404_get_delivery_by_nonexistant_id(self):
-        res = self.client().get('/deliveries/1000000')
+        res = self.client().get('/deliveries/1000000', headers=auth_user)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
     def test_get_orders_by_customer(self):
-        res = self.client().get('/customers/1/orders')
+        res = self.client().get('/customers/1/orders', headers=auth_user)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -178,14 +200,14 @@ class OrdersTestCase(unittest.TestCase):
         self.assertTrue(data['total_orders'])
 
     def test_404_get_orders_by_nonexistant_customer(self):
-        res = self.client().get('/customers/1000000/orders')
+        res = self.client().get('/customers/1000000/orders', headers=auth_user)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
     def test_get_deliveries_by_order(self):
-        res = self.client().get('/orders/1/deliveries')
+        res = self.client().get('/orders/1/deliveries', headers=auth_user)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -194,7 +216,7 @@ class OrdersTestCase(unittest.TestCase):
         self.assertTrue(data['total_deliveries'])
 
     def test_404_get_deliveries_by_nonexistant_order(self):
-        res = self.client().get('/orders/1000000/deliveries')
+        res = self.client().get('/orders/1000000/deliveries', headers=auth_user)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
@@ -203,7 +225,7 @@ class OrdersTestCase(unittest.TestCase):
     # Test Patch
 
     def test_update_customer(self):
-        res = self.client().patch('/customers/1', json={'name': 'Walla Wanga'})
+        res = self.client().patch('/customers/1', headers=auth_admin, json={'name': 'Walla Wanga'})
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -211,7 +233,7 @@ class OrdersTestCase(unittest.TestCase):
         self.assertTrue(data['customer'])
 
     def test_update_order(self):
-        res = self.client().patch('/orders/1', json={'value': 99.99})
+        res = self.client().patch('/orders/1', headers=auth_admin, json={'value': 99.99})
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -219,21 +241,21 @@ class OrdersTestCase(unittest.TestCase):
         self.assertTrue(data['order'])
 
     def test_404_update_customer(self):
-        res = self.client().patch('/customers/10000000', json={'name': 'Jodie Foster'})
+        res = self.client().patch('/customers/10000000', headers=auth_admin, json={'name': 'Jodie Foster'})
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
     def test_404_update_order(self):
-        res = self.client().patch('/orders/10000000', json={'value': 99.99})
+        res = self.client().patch('/orders/10000000', headers=auth_admin, json={'value': 99.99})
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
     def test_405_update_delivery(self):
-        res = self.client().patch('/deliveries/1', json={'order': 1})
+        res = self.client().patch('/deliveries/1', headers=auth_admin, json={'order': 1})
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 405)
@@ -242,7 +264,7 @@ class OrdersTestCase(unittest.TestCase):
     # Test Delete
 
     def test_delete_order(self):
-        res = self.client().delete('/orders/3')
+        res = self.client().delete('/orders/3', headers=auth_admin)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -250,7 +272,7 @@ class OrdersTestCase(unittest.TestCase):
         self.assertTrue(data['deleted'])
 
     def test_delete_delivery(self):
-        res = self.client().delete('/deliveries/3')
+        res = self.client().delete('/deliveries/3', headers=auth_admin)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -258,21 +280,21 @@ class OrdersTestCase(unittest.TestCase):
         self.assertTrue(data['deleted'])
 
     def test_404_delete_order(self):
-        res = self.client().delete('/orders/100000000')
+        res = self.client().delete('/orders/100000000', headers=auth_admin)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
     def test_404_delete_delivery(self):
-        res = self.client().delete('/deliveries/100000000')
+        res = self.client().delete('/deliveries/100000000', headers=auth_admin)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
     def test_405_delete_customer(self):
-        res = self.client().delete('/customers/1000000000')
+        res = self.client().delete('/customers/1000000000', headers=auth_admin)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 405)
@@ -280,4 +302,5 @@ class OrdersTestCase(unittest.TestCase):
 
 
 if __name__ == '__main__':
+
     unittest.main()
